d9047469bbc433723df9bdae82e8abaa92d5a45b
Chimbila2 <89110954+Chimbila2@users.noreply.github.com>
2023-02-02 17:54:15 -0500

    Add files via upload

---
+++ WhatTime/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter the Currently time: ");
+string time = Console.ReadLine();
+Console.WriteLine("the time is: " + time);
+
